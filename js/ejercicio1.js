fa0ba6e6857738cac6b59d9d1a5e57aba0071f9a
gabryi93 <gabryi.gr@gmail.com>
2022-09-08 19:01:08 +0200

    Cambios realizados

---
+++ js/ejercicio1.js
@@ -0,0 +1,25 @@
+var name = "Gabriel"
+console.log(name);
+// name =1;
+console.log (name);
+var variable = null;
+
+//TIPOS DE VARIABLES
+var texto = "Hola" //->tipo string
+var numero = 9 //->tipo number
+var boolean = false;  //->tipo booleano true or false
+var nulo = null;  //->tipo null, se ha cerado pero no se le ha asignado ningun valor
+var noDefinido = undefined;  //->tipo undefined, que isgnifica que esa variable nunca se ha creado
+
+
+//tipo STRING
+
+var texto = 'Holaaaaaaaaaaa"dffffwerfw"aa';
+texto = "Holaaaaaaaaaaa'dffffwerfw'aa";
+texto = "Holaaaaaaaaaaa "+name+"deededededaa";
+var pi = 3.14; //->Siempre con puntos los decimales
+console.log(texto)
+
+
+
+
---
+++ js/index.js
@@ -0,0 +1,20 @@
+//crear variable test
+var test;
+
+test = "Alumno";
+// test = 9;
+
+//en la misma linea creo una var y le asigno valor
+var variableNueva = 'hola';
+// console.log(variableNueva)
+// console.log(test)
+
+var prueba = "primer valor";
+console.log(prueba)
+
+prueba = "segundo valor";
+console.log(prueba)
+
+prueba = 100;
+console.log(prueba)
+
